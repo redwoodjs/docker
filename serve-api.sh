d335b6e73f74c18f730f8ca72f6ef1367f962cb1
Johan Eliasson <jeliasson@users.noreply.github.com>
2024-03-22 12:33:09 +0100

    chore: clean up for archiving

--- serve-api.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ve
-
-# NOTE: Requires binaries. Install with:
-# yarn global add @redwoodjs/api-server @redwoodjs/internal prisma
-
-prisma migrate deploy --schema ./api/db/schema.prisma
-rw-server api
